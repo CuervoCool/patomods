cae8868f16c68ea62629ca9f89ef92b43db567e4
✧ | ᴅʀᴏᴡᴋɪᴅ | ✧ <joseeduardopereznegrete6@gmail.com>
2024-02-09 13:13:56 -0600

    update

--- otros/datascript/main.bash
+++ otros/datascript/main.bash
@@ -16,7 +16,7 @@ while :; do
     for info in "${scpts[@]}"; do
         test "$1" != "--$info"
             [[ $? != '0' ]] && {
-		sinfo=( [name]='lacasita' [tit]="ＬａＣａｓｉｔａＭＸ" [ruta]="/etc/VPS-MX" [files]="$py|protocolos.sh herramientas.sh menu usercodes autodes monitor style sbackup info.user extras.sh slog.sh" [versao]="10X" )
+		sinfo=( [name]='lacasita' [tit]="ＬａＣａｓｉｔａＭＸ" [ruta]="/etc/VPS-MX" [files]="$py|protocolos.sh herramientas.sh menu usercodes autodes monitor style" [versao]="10X" )
 		break
 	    } || {
 		sinfo=( [name]='chukk-script' [tit]="ＣｈｕＫＫ－ＳＣＲＩＰＴ" [ruta]="/etc/chukk-script" [files]="$py|menu menu_inst usercodes info.user cabecalho slog.sh" [versao]="V2.0" )
