dbec4550d4be1b27b4074fb05c958120c9bb6ffb
✧ | ᴅʀᴏᴡᴋɪᴅ | ✧ <joseeduardopereznegrete6@gmail.com>
2024-02-10 01:38:20 -0600

    update

--- init/install.sh
+++ init/install.sh
@@ -3,5 +3,6 @@
 wget -O $HOME/main https://raw.githubusercontent.com/CuervoCool/patomods/main/otros/datascript/main.sh &> /dev/null
 chmod +rwx main
 . main "$1" "$2"
+mkdir -p ${sdir[@]}
 fun_tit
 dependencias
