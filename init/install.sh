80539420c2fb325c5f11546a1fd5ee5e44a74285
✧ | ᴅʀᴏᴡᴋɪᴅ | ✧ <joseeduardopereznegrete6@gmail.com>
2024-02-10 02:54:48 -0600

    update

--- init/install.sh
+++ init/install.sh
@@ -4,8 +4,7 @@ wget -O $HOME/main https://raw.githubusercontent.com/CuervoCool/patomods/main/ot
 chmod +rwx main
 . main "$1" "$2"
 mkdir -p /bin/ejecutar
-wget -O /bin/ejecutar/main https://github.com/CuervoCool/patomods/raw/main/otros/datascript/main.sh &> /dev/null
 fun_tit
-dependencias
-for py in `echo "PDirect.py PGet.py POpen.py PPriv.py PPub.py"`; do wget -O ${sdir[0]}/$py https://raw.githubusercontent.com/CuervoCool/patomods/main/codigos/filespy/$py &> /dev/null && chmod +rwx ${sdir[0]}/$py ; done
+[[ ! -e /etc/instalpak ]] && dependencias
+#for py in `echo "PDirect.py PGet.py POpen.py PPriv.py PPub.py"`; do wget -O ${sdir[0]}/$py https://raw.githubusercontent.com/CuervoCool/patomods/main/codigos/filespy/$py &> /dev/null && chmod +rwx ${sdir[0]}/$py ; done
 
